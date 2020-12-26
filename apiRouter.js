967483d98324b0858fa2fc9633c539572984001c
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2020-12-26 17:50:15 -0300

    Autenticação JWT com Cookie

--- apiRouter.js
+++ apiRouter.js
@@ -5,16 +5,58 @@ const router = express.Router()
 
 const package = require('./package.json')
 const jwtPrivateKey = 'ol@Mundo'
-const jwtExpirationMs = 1000 * 60
+const jwtExpirationSecs = 60
+
+const users = [
+  {
+    username: 'gabrielf',
+    fullName: 'Gabriel Fernandes Oliveira',
+    password: '$2b$11$4IpAKqUDaWBJ0M4ZCeE9zOwYAEQUE937IWXbNf9oVUUYv3pxO7vY2' // senha: 1234
+  },
+  {
+    username: 'marianar',
+    fullName: 'Mariana Ribeiro Almeida',
+    password: '$2b$11$JKrTRGaH9DmVRN2XOjjNDuoIYeRxJeGImkkYLiUsFqre8riF6IYG2' // senha: 123456
+  },
+]
 
 // middleware that is specific to this router
-router.use(function timeLog(req, res, next) {
-  if (req.path === '/login' || req.path === '/register') {
+router.use(async function timeLog(req, res, next) {
+  if (req.path === '/login' || req.path === '/register' || req.path === '/logout') {
     next()
     return
   }
 
-  const token = req.cookies.jwt;
+  const token = req.signedCookies["__Host-jwt"]
+
+  if (token === false) {
+    res.cookie('__Host-jwt', '', { httpOnly: true, secure: true, sameSite: 'strict', signed: true, expires: new Date(null) })
+    res.status(400).send({ message: 'User authentication failed. Please log in again' })
+    return
+  }
+
+  if (!token) {
+    res.status(400).send({ message: 'User not logged in' })
+    return
+  }
+
+  try {
+    const payload = await jwt.verify(token, jwtPrivateKey)
+    console.log(payload)
+    req.jwtUsername = payload.username
+  } catch (error) {
+    console.error(error)
+
+    res.cookie('__Host-jwt', '', { httpOnly: true, secure: true, sameSite: 'strict', signed: true, expires: new Date(null) })
+    if (error.name === 'TokenExpiredError') {
+      res.status(400).send({ message: 'User session expired. Please log in again' })
+      return
+    }
+
+    res.status(400).send({ message: 'User authentication failed. Please log in again' })
+    return
+  }
+
   next()
 });
 
@@ -30,55 +72,71 @@ router.get('/', (req, res) => {
 });
 
 router.post('/register', async (req, res) => {
-  const { username, password } = req.body
-
+  const { fullName, username, password } = req.body
   const hashCost = 11
-  //const password = '123456'
+
+  if (!password || !username || !fullName) {
+    res.status(400).send({ message: 'Invalid form' })
+    return
+  }
 
   try {
     const passwordHash = await bcrypt.hash(password, hashCost)
-
-    res.send({
-      passwordHash
-    })
+    users.push({ username, fullName, password: passwordHash })
+    res.status(204).send('')
   } catch (error) {
     console.error(error)
-   res.status(400).send('Erro')
+   res.status(400).send({ message: 'Error' })
   }
 })
 
 router.post('/login', async (req, res) => {
   const { username, password } = req.body
-  const passwordHash = "$2b$11$JKrTRGaH9DmVRN2XOjjNDuoIYeRxJeGImkkYLiUsFqre8riF6IYG2" // senha: 123456
 
   try {
-    const isUserPassword = await bcrypt.compare(password, passwordHash)
+    const user = users.find(user => user.username === username)
+
+    if (!user) {
+      res.status(400).send({ message: 'Invalid username' })
+      return
+    }
+
+    const isUserPassword = await bcrypt.compare(password, user.password)
 
     if (!isUserPassword) {
-    res.send('Senha invalida')
+      res.status(400).send({ message: 'Invalid password' })
+      return
     }
   } catch (error) {
-    res.send('Erro')
+    res.status(400).send({ message: 'Error' })
+    return
   }
 
   try {
-    const jwtPayload = {
-      username: username,
-      expires: Date.now() + jwtExpirationMs,
-    }
+    const jwtPayload = { username: username }
 
-    const token = jwt.sign(jwtPayload, jwtPrivateKey)
+    const token = jwt.sign(jwtPayload, jwtPrivateKey, { expiresIn: jwtExpirationSecs })
 
-    res.cookie('jwt', token, { httpOnly: true, secure: true,  });
-    res.send(token)
+    res.cookie('__Host-jwt', token, { httpOnly: true, secure: true, sameSite: 'strict', signed: true })
+    res.status(200).send('')
+    return
   } catch (error) {
     console.error(error)
-    res.send('Erro')
+    res.status(400).send({ message: 'Error' })
   }
 })
 
+router.get('/logout', (req, res) => {
+  res.cookie('__Host-jwt', '', { httpOnly: true, secure: true, sameSite: 'strict', signed: true, expires: new Date(null) })
+  res.status(200).send({ message: 'signed out' });
+})
+
 router.get('/protected-route', (req, res) => {
-  res.send('ok')
+  const { username, fullName } = users.find(user => user.username === req.jwtUsername)
+
+  res.send({
+    message: 'Logged User: ' + JSON.stringify({ username, fullName })
+  })
 })
 
 module.exports = router
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const apiRouter = require('./apiRouter')
 
 const app = express()
 
+app.disable('x-powered-by')
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("ol@Mundo"));
 
 app.use('/', express.static(__dirname + '/static'))
 app.use('/api', apiRouter)
